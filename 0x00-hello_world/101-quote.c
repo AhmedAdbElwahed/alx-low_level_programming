d3e1be2897f87ce9664acc5b880379fcdcfa7655
ahmed <ahmedabdelwhed68@gamil.com>
2023-06-16 02:00:18 +0300

    add advanced task file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints string using sterr
+ * Return: 1 if the program fail. 0 if the program success
+*/
+int main(void)
+{
+char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, buf, sizeof(buf));
+return (1);
+}
