454c3da2fd068dd8c0a03c657cf8f5c9975297c8
ahmed <ahmedabdelwhed68@gamil.com>
2023-06-21 02:34:09 +0300

    fix some typoes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	char name[] = "_putchar";
 	int i;
-	for (i = 0; i < sizeof(name); i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(name[i]);
 	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,4 @@
 #include <stdio.h>
 int _putchar(char c);
 
-#endif //FUCNTIONS_MAIN_H
+#endif
