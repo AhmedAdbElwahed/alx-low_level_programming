1b07d2bb5435b1aadfad27ba31a7f6115d88a761
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-06-21 05:05:57 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,3 +12,14 @@ int print_last_digit(int n)
 	_putchar(last_digit);
 	return (last_digit - 48);
 }
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: integer number
+ * Return: the absolute of a given number
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	return (n * -1);
+}
