5d2f8ed81d7af4647bca9082eef722bf0d2b18b6
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-06-22 02:27:14 +0300

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ * @n: the limit of the tabel.
+ */
+void times_table(int n)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i = 0; i < n + 1; i++)
+	{
+		_putchar('0');
+		for (j = 1; j < n + 1; j++)
+		{
+			result = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (result < 10)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + 48);
+			_putchar((result % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
