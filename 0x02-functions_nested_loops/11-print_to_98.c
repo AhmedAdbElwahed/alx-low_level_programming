e6a2fca13ee1f2605a032b2a97efdb161e50b904
ahmed <ahmedabdelwhed68@gamil.com>
2023-06-21 05:25:04 +0300

    add another file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void _print(int n);
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * , followed by a new line.
+ * @n: the start number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_print(i);
+		if (i == 98)
+			break;
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+
+}
+
+/**
+ * _print - prints integer number using putchar func
+ * @n: integer number to be printed
+ */
+void _print(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print(n / 10);
+
+	_putchar(n % 10 + '0');
+}
