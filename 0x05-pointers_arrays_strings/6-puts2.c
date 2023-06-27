1d4be5f5074a80e86d27660f70d18b3163b40948
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-06-27 04:46:08 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-int my_strlen3(char *s);
 /**
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
