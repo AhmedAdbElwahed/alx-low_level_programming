8c1877424163ac926ef9eb278dfc5ab35f0d8d91
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-07-04 05:21:25 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,24 @@
-#include <string>
 #include "main.h"
+
+/**
+ * my_strchr - a function that locates a character in a string.
+ * @s: the string to be searched.
+ * @c: the char to be found.
+ * Return: a pointer to the first occurrence
+ * of the character c in the string s,
+ * or NULL if the character is not found
+ */
+char *my_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+return (NULL);
+}
 
 /**
  * _strspn - a function that gets the length of a prefix substring.
@@ -17,7 +36,7 @@ if ((s == NULL) || (accept == NULL))
 return (len);
 }
 
-while (*s && strchr(accept, *s++))
+while (*s && my_strchr(accept, *s++))
 {
 len++;
 }
