70788e814a2604a38090153a0c82177bfab326c5
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-07-04 05:24:01 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,25 @@
 #include "main.h"
+
+/**
+ * my_strchr - a function that locates a character in a string.
+ * @s: the string to be searched.
+ * @c: the char to be found.
+ * Return: a pointer to the first occurrence
+ * of the character c in the string s,
+ * or NULL if the character is not found
+ */
+char *my_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+return (NULL);
+}
+
 /**
  * _strstr - a function that locates a substring.
  * @haystack: the string to be searched.
@@ -14,7 +35,7 @@ return (NULL);
 }
 while (*haystack)
 {
-if (_strchr(needle, *haystack))
+if (my_strchr(needle, *haystack))
 {
 return (haystack);
 }
