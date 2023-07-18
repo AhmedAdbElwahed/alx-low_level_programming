9dea6967eff6e888d0efe155b66d0a8875ca2488
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-07-18 18:53:02 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef PREPROCESSOR_3_FUNCTION_LIKE_MACRO_H
 #define PREPROCESSOR_3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) >= 0 ? (x) : (x) * -1 )
+#define ABS(x) ((x) >= 0 ? (x) : (-(x)))
 
 #endif
