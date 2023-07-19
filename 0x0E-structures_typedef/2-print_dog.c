f7b8ec8cf524edd87281bfec0b8c1c4d2f6a1a9e
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-07-19 03:18:32 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,35 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
-char *nil = "(nil)";
-
 if (d)
 {
-if (d->name != NULL)
-{
-printf("Name: %s\n", d->name);
-}
-else
-{
-printf("Name: %s\n", nil);
-}
-
-if (d->age != 0)
-{
+printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
-}
-else
-{
-printf("Age: %s\n", nil);
-}
-
-if (d->owner != NULL)
-{
-printf("Owner: %s\n", d->owner);
-}
-else
-{
-printf("Owner: %s\n", nil);
-}
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
