cf64e53abbbb0e6a061327e03b4e1634b312b268
AhmedAdbElwahed <83559587+AhmedAdbElwahed@users.noreply.github.com>
2023-08-08 04:53:54 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int _strlen(const char *str)
  * @text_content: a NULL terminated string to write to the file.
  * Return: 1 on success, -1 on failure.
  */
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0;
